340a13461d3f1612daecb6ffa77f4cd733eb3b78
Mariano-Solis <105520905+Mariano-Solis@users.noreply.github.com>
2026-09-02 22:31:56 -0300

    fix: load official MGSM logo from bundled asset

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import officialLogo from '../Logo.jpg'
 import './styles.css'
 import './mgsm-topbar.css'
+import './mgsm-logo-assets.css'
+
+document.documentElement.style.setProperty('--mgsm-logo-url', `url("${officialLogo}")`)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>
