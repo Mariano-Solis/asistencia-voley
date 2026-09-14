1c2a3c83289ea6d9e93444247873f39ab2cd269e
Mariano-Solis <105520905+Mariano-Solis@users.noreply.github.com>
2026-09-14 19:03:43 -0300

    Mount header position enhancer for Solapas

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import PaymentHubStable from './PaymentHubStable'
 import AdminDualTopbarAction from './AdminDualTopbarAction'
 import FeatureTabManager from './FeatureTabManager'
 import PasswordVisibilityEnhancer from './PasswordVisibilityEnhancer'
+import FeatureTabHeaderMover from './FeatureTabHeaderMover'
 import officialLogo from '../Logo.jpg'
 import './styles.css'
 import './mgsm-topbar.css'
@@ -55,5 +56,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <AdminDualTopbarAction />
     <FeatureTabManager />
     <PasswordVisibilityEnhancer />
+    <FeatureTabHeaderMover />
   </>
 )
