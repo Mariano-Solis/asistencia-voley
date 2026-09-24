2a8daf43d4256fed5e3736e9d497484073304d61
Mariano-Solis <105520905+Mariano-Solis@users.noreply.github.com>
2026-09-24 01:54:22 -0300

    Cargar estilos de asistencia limpia

--- src/main.jsx
+++ src/main.jsx
@@ -63,6 +63,7 @@ import './mgsm-multi-category-filter.css'
 import './mgsm-player-export.css'
 import './mgsm-selfie-preview.css'
 import './mgsm-professors-compact.css'
+import './mgsm-player-attendance-clean.css'
 
 document.documentElement.style.setProperty('--mgsm-logo-url', `url("${officialLogo}")`)
 
